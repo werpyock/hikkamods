487858a76fb59e7a449d92aa545af162a72456bb
werpyock <ttar2571@gmail.com>
2025-02-04 17:36:51 +0400

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,4 +1,4 @@
-__version__ = (1, 0, 1)
+__version__ = (1, 0, 2)
 # meta developer: @werpyock0
 from hikka import loader, utils
 import asyncio
@@ -14,19 +14,19 @@ class IrisMod(loader.Module):
 
     @loader.command()
     async def iris(self, message):
-        """Отправляет команду @iris_cm_bot."""
+        """Отправляет команду @iris_black_bot."""
         command_text = utils.get_args_raw(message)
         if not command_text:
-            await message.edit("<b>❌ Укажите команду для @iris_cm_bot</b>")
+            await message.edit("<b>❌ Укажите команду для @iris_black_bot</b>")
             return
 
-        bot_username = "iris_dark_bot"
+        bot_username = "iris_black_bot"
         try:
             async with self.client.conversation(bot_username, timeout=15) as conv:
                 await conv.send_message(command_text)
                 response = await conv.get_response()
                 await message.edit(response.text)
         except TimeoutError:
-            await message.edit("<b>❌ Бот не ответил в течение 15 секунд, задача отменена</b>")
+            await message.edit("<b>❌ Бот не ответил в течение 15 секунд</b>")
         except Exception as e:
             await message.edit(f"<b>❌ Ошибка: {e}</b>")
