76afb8c2c01111caaec01219ad64392b3471573b
werpyock <ttar2571@gmail.com>
2025-02-04 15:13:38 +0400

    Update komarumod.py

--- komarumod.py
+++ komarumod.py
@@ -1,16 +1,18 @@
-__version__ = (1, 8, 0)
+__version__ = (1, 9, 0)
 # meta developer: @werpyock0
+
 from .. import loader, utils
 import random
 from telethon.tl.types import InputMessagesFilterGif
 
 class KomaruMod(loader.Module):
     """Достает рандомную гифку из @komarumod"""
-    
+
     strings = {"name": "KomaruMod"}
 
     async def client_ready(self, client, db):
         self.client = client
+
     async def komarugifcmd(self, message):
         """Рандомная гифка"""
         channel = "@komarumod"
@@ -18,6 +20,14 @@ class KomaruMod(loader.Module):
         if not gifs:
             await message.edit("<b>Нет доступных GIF-ов в канале!</b>")
             return
+
         random_gif = random.choice(gifs)
-        await self.client.send_file(message.chat_id, random_gif)
+        reply_to = message.reply_to_msg_id
+
+        await self.client.send_file(
+            message.chat_id,
+            random_gif,
+            reply_to=reply_to
+        )
+
         await message.delete()
