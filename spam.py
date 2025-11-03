0eb39991d3320a56cd0b93108d8fa0a649c81bf7
Werpyock <193580448+werpyock@users.noreply.github.com>
2025-11-03 15:30:40 +0400

    Update meta developer in spam.py

--- spam.py
+++ spam.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 __version__ = (1, 4, 1)
-# meta developer: @wmodules
+# meta developer: @terrasa120
 from .. import loader, utils
 import asyncio
 import shlex
